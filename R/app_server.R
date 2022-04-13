c0b4528d33ccc2a60177d048dd59694cbec2e56c
Marcelo Ribeiro <mrodriguesribeiro@gmail.com>
2022-04-13 11:23:59 -0300

    Subindo uma pasta

--- R/app_server.R
+++ R/app_server.R
@@ -7,9 +7,9 @@
 app_server <- function(input, output, session) {
     
     output$network <- renderVisNetwork({
-      load("data/nodes.rda") # conexão com um banco
+      load("../data/nodes.rda") # conexão com um banco
 
-      load("data/edges.rda")
+      load("../data/edges.rda")
       
 
       visNetwork(nodes, edges) %>%
